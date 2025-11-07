ca73369d323126ed765936662be8841abe7e496f
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-11-07 22:21:37 +0000

    Handle element creation on the client

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Loaded: script.js");
+
+// get the tracklist from json data sent by the server (thanks server much appreciated)
+const musicData = JSON.parse(document.querySelector("#music-data").textContent);
+const trackList = musicData.trackList;
+
+const audioPlayer = document.querySelector("audio");
+
+for (let trackName of trackList) {
+    const span = document.createElement("span");
+    span.innerText = trackName;
+    span.onclick = function () {
+        audioPlayer.src = trackName;
+        audioPlayer.play();
+    };
+    const br = document.createElement("br");
+    document.body.appendChild(span);
+    document.body.appendChild(br);
+}
--- tools/serve.js
+++ tools/serve.js
@@ -22,14 +22,16 @@ const mimeTypes = {
     ".m4a": "audio/mp4",
 };
 
-function createAudioElements(musicFiles) {
+function createMusicElements(musicFiles) {
     if (!Array.isArray(musicFiles))
-        console.error("createAudioElements expected array but got non-array");
+        console.error("createMusicElements expected array but got non-array");
 
     let elements = "";
 
     for (let file of musicFiles) {
-        elements += `<audio controls src="${file}"></audio><br>`;
+        // we shouldnt be writing functions inside strings
+        // this is just placeholder logic
+        elements += `<span onclick="const audio = document.querySelector('audio'); audio.src = '${file}'; audio.play(); audio.focus();">${file}</span><br>`;
     }
 
     return elements;
@@ -39,7 +41,12 @@ function formatHTML(baseHTML, data) {
     if (!data.music) console.error("no music was supplied");
     return baseHTML
         .toString()
-        .replaceAll("<!-- <music> -->", createAudioElements(data.music));
+        .replaceAll(
+            "<!-- <music> -->",
+            JSON.stringify({
+                trackList: data.music,
+            }) /* createMusicElements(data.music) */
+        );
 }
 
 app.use(express.json()); // parse json
